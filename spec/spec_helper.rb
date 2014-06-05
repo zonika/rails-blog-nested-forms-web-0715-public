c3891b5c156d88ae55bdd620be22ce9626dc209d
ahimmelstoss <ahimmelstoss@gmail.com>
2014-06-05 12:11:37 -0400

    added tests for nested forms

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,7 +19,16 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "default"
   config.include Capybara::DSL
+  
   DatabaseCleaner.strategy = :truncation
+
+  config.before(:each) do 
+    @crookshanks = User.create(name: "Crookshanks")
+    @kitten = User.create(name: "Kitten")
+    @post1 = @crookshanks.posts.create(name: "post title", content: "post content")
+    @post2 = @crookshanks.posts.create(name: "my second post", content: "post content")
+  end
+
   config.after(:all) do 
     DatabaseCleaner.clean
   end
